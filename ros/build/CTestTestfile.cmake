ab55b1a0eb3e8865bd0f3a981e2ff141a9cd74b0
towardsautonomy <shubham@TowardsAutonomy.com>
2021-04-25 19:16:38 -0700

    setup ros melodic

--- ros/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jetson/JetAutonomy/ros/src
-# Build directory: /home/jetson/JetAutonomy/ros/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
